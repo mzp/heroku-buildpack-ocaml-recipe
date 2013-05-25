56c87f8776ee475c8c1d840cd568cc36aa02b94a
mzp <mzpppp@gmail.com>
2013-05-25 11:58:47 +0400

    add eliom recipe

--- ocaml/build.sh
+++ ocaml/build.sh
@@ -2,4 +2,4 @@
 wget http://caml.inria.fr/pub/distrib/ocaml-4.00/ocaml-4.00.1.tar.gz
 tar xvzf ocaml-4.00.1.tar.gz
 cd ocaml-4.00.1
-vulcan build -p /tmp/ocaml -v -c './configure -prefix /tmp/ocaml && make world.opt && make install'
+vulcan build -p /app/vendor/ocaml -v -c './configure -prefix /app/vendor/ocaml && make world.opt && make install'
--- opam/build.sh
+++ opam/build.sh
@@ -2,4 +2,4 @@
 wget http://www.ocamlpro.com/pub/opam-full-1.0.0.tar.gz
 tar xvzf opam-full-1.0.0.tar.gz
 cd opam-full-1.0.0
-vulcan build -p /tmp/opam -v -c './setup && export PATH=/tmp/ocaml/bin:$PATH && ./configure --prefix=/tmp/opam && make && make install'
+vulcan build -p /app/vendor/opam -v -c './setup && export PATH=/app/vendor/ocaml/bin:$PATH && ./configure --prefix=/app/vendor/opam && make && make install'
